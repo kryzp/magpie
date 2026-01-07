b68669977cc0322fa8f8455fd1a0a06dc5f50eb9
kryzp <kprzeczek4@gmail.com>
2026-01-07 00:22:07 +0000

    Made code not bad anymore.

--- src/graphics/device.cpp
+++ src/graphics/device.cpp
@@ -816,8 +816,6 @@ VkSemaphore Device::get_current_image_available_semaphore()
 
 CommandBuffer Device::begin_frame(Swapchain &swapchain)
 {
-	reset_command_pool(graphics_queue.get_current_sync_data().command_pool);
-
 	VkAcquireNextImageInfoKHR acquire_next_image_info = {};
 	acquire_next_image_info.sType = VK_STRUCTURE_TYPE_ACQUIRE_NEXT_IMAGE_INFO_KHR;
 	acquire_next_image_info.swapchain = swapchain.get_handle();
@@ -833,6 +831,8 @@ CommandBuffer Device::begin_frame(Swapchain &swapchain)
 	else if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR)
 		debug_log_crash("Failed to acquire next image in swapchain.");
 	
+	graphics_queue.reset_command_pool();
+
 	CommandBuffer cmd = graphics_queue.begin_submit(per_frame_data[current_frame_index].in_flight_fence);
 
 	return cmd;
