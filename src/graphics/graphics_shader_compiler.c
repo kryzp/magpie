41ed45dda4aead40934636d40f053e3216990217
kryzp <kprzeczek4@gmail.com>
2026-04-17 12:09:09 +0100

    Shader compiler stuff.

---
+++ src/graphics/graphics_shader_compiler.c
@@ -0,0 +1,8 @@
+
+internal GFX_ShaderCompilerAPI
+GFX_GetShaderCompilerAPI(void)
+{
+	static GFX_ShaderCompilerAPI api = {0};
+
+	return api;
+}
--- src/graphics/graphics_shader_compiler.h
+++ src/graphics/graphics_shader_compiler.h
@@ -1,14 +1,30 @@
 #ifndef GRAPHICS_SHADER_COMPILER_H
 #define GRAPHICS_SHADER_COMPILER_H
 
-typedef struct GFX_ShaderCompiler GFX_ShaderCompiler;
-struct GFX_ShaderCompiler
+typedef struct GFX_CompiledShaderStage GFX_CompiledShaderStage;
+struct GFX_CompiledShaderStage
+{
+	GFX_ShaderBytecode bytecode;
+	u32 push_constant_size;
+	VkShaderStageFlags stage;
+};
+
+typedef struct GFX_CompiledShaderProgram GFX_CompiledShaderProgram;
+struct GFX_CompiledShaderProgram
+{
+	u32 stage_count;
+	GFX_CompiledShaderStage *stages;
+	b32 failed;
+};
+
+typedef struct GFX_ShaderCompilerAPI GFX_ShaderCompilerAPI;
+struct GFX_ShaderCompilerAPI
 {
 	void (*Init)(void);
 	void (*Shutdown)(void);
 	GFX_ShaderProgram (*Compile)(String8 source_path, u32 search_path_count, const String8 *search_paths, b32 *failed);
 };
 
-GFX_ShaderCompiler GFX_GetShaderCompiler();
+internal GFX_ShaderCompilerAPI GFX_GetShaderCompilerAPI(void);
 
 #endif // GRAPHICS_SHADER_COMPILER_H
